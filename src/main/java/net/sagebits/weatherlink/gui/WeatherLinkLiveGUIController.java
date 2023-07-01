4ff8183cc6204948cf02e6d5b1488babcc826bd0
semigator <136933281+semigator@users.noreply.github.com>
2023-06-30 22:16:33 -0400

    Revert "Update WeatherLinkLiveGUIController.java"
    This reverts commit c0a9b0890a44cc9b55af061cd24f24be54310104.

--- src/main/java/net/sagebits/weatherlink/gui/WeatherLinkLiveGUIController.java
+++ src/main/java/net/sagebits/weatherlink/gui/WeatherLinkLiveGUIController.java
@@ -792,7 +792,7 @@ public class WeatherLinkLiveGUIController
 			{
 				t.setText(Math.round(1 / uvindex.asDouble().multiply(.428571).get() * 60) + " Minutes to Burn");
 			}
-			else
+			if (thisone = 0)
 			{
 				updateTooltip.accept(null);
 				
